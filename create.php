1391cf56919cf8165d7dd6b309b85951e000a6be
dimonkiss <dklxwa@gmail.com>
2023-12-05 03:40:27 +0200

    Update create.php

--- create.php
+++ create.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="uk">
 <head>
     <meta charset="UTF-8">
@@ -17,29 +17,44 @@
 
     <h1 class="text-center">Додати категорію</h1>
 
-    <form class="col-md-6 offset-md-3">
+    <form class="col-md-6 offset-md-3" id="categoryForm">
         <div class="mb-3">
             <label for="name" class="form-label">Назва</label>
-            <input type="text" class="form-control" name="name" id="name" >
+            <input type="text" class="form-control" name="name" id="name">
         </div>
 
         <div class="row">
             <div class="col-md-4">
                 <img src="https://t4.ftcdn.net/jpg/04/70/29/97/360_F_470299797_UD0eoVMMSUbHCcNJCdv2t8B2g1GVqYgs.jpg"
-                     alt="Обране фото" width="100%">
+                     alt="Обране фото" width="100%" id="selectedImage">
             </div>
             <div class="col-md-8">
                 <div class="mb-3">
                     <label for="image" class="form-label">Оберіть фото</label>
-                    <input class="form-control" type="file" id="image" name="image" accept="image/*">
+                    <input class="form-control" type="file" id="image" name="image" accept="image/*" onchange="displaySelectedImage()">
                 </div>
             </div>
         </div>
 
         <button type="submit" class="btn btn-primary">Додати</button>
     </form>
-12
 </div>
 <script src="/js/bootstrap.bundle.min.js"></script>
+<script>
+    function displaySelectedImage() {
+        var input = document.getElementById('image');
+        var selectedImage = document.getElementById('selectedImage');
+
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                selectedImage.src = e.target.result;
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+</script>
 </body>
 </html>
